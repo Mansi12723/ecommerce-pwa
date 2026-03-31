01ba86b9adc3735c6831de2fb89bd3f114878228
Mansi12723 <mansizalavadia@acpce.ac.in>
2026-03-31 15:09:01 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "ecommerce-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/ecommerce-pwa/",
+  "/ecommerce-pwa/index.html",
+  "/ecommerce-pwa/app.js",
+  "/ecommerce-pwa/manifest.json"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(names => {
+      return Promise.all(
+        names.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
+});
